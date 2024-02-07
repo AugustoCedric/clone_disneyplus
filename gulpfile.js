a9288b27becfdb95d399575f04d57078685ace4c
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-02-07 18:56:25 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const sass = require("gulp-dart-sass");
+const imagemin = require("gulp-imagemin");
 
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./dist/css")); // Substitua o caminho de destino conforme necessário
+    .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+// Adicione a tarefa "build" para incluir as tarefas de styles e images
+exports.build = gulp.parallel(styles, images);
+
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
+
+// Mantenha a tarefa padrão para "npm run dev"
+exports.default = exports.watch;
