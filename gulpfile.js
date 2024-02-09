09878959736eb72c5b23cb10153b6a58dda5ee94
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-02-09 14:34:16 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require("gulp");
-const sass = require("gulp-dart-sass");
+const sass = require("gulp-sass")(require("sass"));  // Adicione o compilador Sass aqui
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function scripts() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
 
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(gulp.dest("./dist/css"));
 }
 
@@ -16,12 +24,11 @@ function images() {
     .pipe(gulp.dest("./dist/images"));
 }
 
-// Adicione a tarefa "build" para incluir as tarefas de styles e images
-exports.build = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
-  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/styles/**/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scripts));
 };
 
-// Mantenha a tarefa padrão para "npm run dev"
-exports.default = exports.watch;
+
